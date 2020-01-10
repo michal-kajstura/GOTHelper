e038acefc65cbd1e91e35bde9424a2f3001401da
michalkajstura <bayaog98>
2020-01-10 10:30:25 +0100

    initial

---
+++ src/test/java/help/got/GotApplicationTests.java
@@ -0,0 +1,13 @@
+package help.got;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
